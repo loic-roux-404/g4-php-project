1ed71d64e5a613b60277c792b572f4d9ab89adb6
julien ratahiry <julien.ra@live.fr>
2019-09-25 18:23:27 +0200

    Les formulaires

--- src/index.php
+++ src/index.php
@@ -20,6 +20,74 @@ include 'bootstrap.php';
     <div class="container">
         <?php include(VF . "navbar.php"); ?>
 
+        <h2>Les structures</h2>
+        <form action="">
+        
+        <table  class=" table table-striped"   style="border: 3px solid black; border-radius: 10px  " >
+                        <thead>
+                            <tr>
+
+                                <th>Id</th>
+                                <th>Nom</th>
+                                <th>Rue</th>
+                            </tr>
+                        </thead>
+                        <tbody class="mygrid-wrapper-div">
+                            <?php
+// pour chaque ligne (chaque enregistrement)
+
+                            foreach ($objet as $row) {
+
+// DONNEES A AFFICHER dans chaque cellule de la ligne
+                                ?>
+                                <tr>
+                                    <td><?php echo $row['ID']; ?></td>
+                                    <td><?php echo $row['NOM']; ?></td>
+                                    <td><?php echo $row['RUE']; ?></td>
+                                    <td>
+                                        <button  name="id" value="<?php echo $row['ID']; ?>" class="btn btn-info">detail</button>
+                                    </td>
+                                </tr>
+                                <?php
+                            } // fin foreach
+                            ?>    
+                        </tbody>
+                    </table>  
+                </form>
+
+                <h2>Les secteurs</h2>
+        <form action="">
+        
+        <table  class=" table table-striped"   style="border: 3px solid black; border-radius: 10px  " >
+                        <thead>
+                            <tr>
+
+                                <th>Id</th>
+                                <th>Libelle</th>
+                                
+                            </tr>
+                        </thead>
+                        <tbody class="mygrid-wrapper-div">
+                            <?php
+// pour chaque ligne (chaque enregistrement)
+
+                            foreach ($objet as $row) {
+
+// DONNEES A AFFICHER dans chaque cellule de la ligne
+                                ?>
+                                <tr>
+                                    <td><?php echo $row['ID']; ?></td>
+                                    <td><?php echo $row['LIBELLE']; ?></td>
+                                    <td>
+                                        <button  name="id" value="<?php echo $row['id']; ?>" class="btn btn-info">Détail</button>
+                                    </td>
+                                </tr>
+                                <?php
+                            } // fin foreach
+                            ?>    
+                        </tbody>
+                    </table>  
+                </form>
         <?php include(VF . "footer.php"); ?>
     </div>
 </body>
