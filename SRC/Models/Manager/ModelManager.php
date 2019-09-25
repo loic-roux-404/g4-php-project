49be57f01a05402c37588b9266d923a59557e5b0
loic-roux <69loloro10@gmail.com>
2019-09-25 17:22:33 +0200

    manager functions

--- SRC/Models/Manager/ModelManager.php
+++ SRC/Models/Manager/ModelManager.php
@@ -1,15 +1,72 @@
 <?php
 
 namespace SRC\Models\Manager;
- 
+
 class ModelManager implements IModelManager
 {
-    private $_entity;
+    const SV = "remotemysql.com";
+    const USR = "mVrrTxQ5nS";
+    const PW = "IL3tuUFSCQ";
+    const DB = "mVrrTxQ5nS";
 
-    public function __construct(Entity $_entity)
+    private $_cells;
+
+    public function __construct($_cells)
     {
-        $this->_entity = $_entity;
+        $this->_cells = $_cells;
     }
 
+    public function connect()
+    {
+        try {
+            new PDO("mysql:host=$this::SV", $this::USR, $this::PW);
+        } catch (Exception $e) {
+            return 'Connection failed ' . $e;
+        }
+    }
+    /**
+     * function index
+     * @param string $en
+     * @param array $data
+     * @return void
+     */
+    public function index(string $en, int $id)
+    { }
+    /**
+     * function update
+     * @param string $en
+     * @param array $data
+     * @return void
+     */
 
+    public function update(string $en, int $id, array $data)
+    { }
+
+    /**
+     * function create
+     * @param string $en
+     * @param array $data
+     * @return void
+     */
+    public function create(string $en, array $data): void
+    { }
+    /**
+     * function delete
+     *
+     * @param string $en
+     * @param integer $id
+     * @return void
+     */
+    public function delete(string $en, int $id): void
+    { }
+
+    /**
+     * Set the value of _cells
+     *
+     * @param string $_cells
+     */
+    public function setCells(array $_cells)
+    {
+        $this->_cells = $_cells;
+    }
 }
